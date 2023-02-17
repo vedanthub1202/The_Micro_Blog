3ead88b490fb9ff83f15a0564a944b974f26558f
Vedant <vedant.joshi@walchandsangli.ac.on>
2023-02-18 01:09:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Navbar from './Navbar';
+import BlogDetails from './BlogDetails';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+
+            <Route path='/blogs/:id'>
+              <BlogDetails/>
+            </Route>
+
+            <Route path='*'>
+              <NotFound/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>thath page cannot found</p>
+            <Link to='/'>Back to the homepage..</Link>
+        </div>
+     );
+}
+ 
+export default NotFound;
